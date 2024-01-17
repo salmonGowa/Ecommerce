741ed80dcf975222e922a295abc5276c9e25282a
salmonGowa <gowasalmon@yahoo.com>
2024-01-17 11:14:18 +0300

    frontend

--- ecommerce/ecommerceapp/urls.py
+++ ecommerce/ecommerceapp/urls.py
@@ -4,4 +4,7 @@ from ecommerceapp import views
 
 urlpatterns = [
     path('',views.index,name='index'),
+    path('contact',views.contact,name='contact'),
+    path('about',views.about,name="about")
+    
 ]
